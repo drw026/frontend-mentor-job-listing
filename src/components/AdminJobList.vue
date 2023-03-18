477d7708d0512ceaae24ffab44a2b4fceb9c24d9
Andrew Reasoa <andrew.reasoa@incentro.com>
2023-03-18 23:01:49 +0100

    add loading and remove job

--- src/components/AdminJobList.vue
+++ src/components/AdminJobList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto rounded-md bg-white p-6">
-    <div class="mb-6 pb-6 flex justify-between border-b">
+    <div class="mb-6 flex justify-between border-b pb-6">
       <h1 class="text-3xl font-bold text-turqoise-dark">Manage Jobs</h1>
       <router-link to="/admin/add-job" custom v-slot="{ navigate }">
         <button
@@ -12,7 +12,7 @@
         </button>
       </router-link>
     </div>
-    <ul v-if="jobs.length > 0" class="flex flex-col gap-8">
+    <ul v-if="jobs.length > 0 && !isLoading" class="flex flex-col gap-8">
       <li v-for="job in jobs" class="flex justify-between">
         <div class="flex flex-col">
           <h2 class="text-lg font-bold text-turqoise-dark">
@@ -27,6 +27,7 @@
         </div>
         <div class="self-center">
           <button
+            @click="removeJob(job.id)"
             class="rounded bg-turqoise-light px-3 py-2 font-bold text-turqoise hover:bg-turqoise hover:text-white"
           >
             Remove
@@ -34,11 +35,13 @@
         </div>
       </li>
     </ul>
+    <span v-else-if="isLoading">Loading...</span>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { timer, sleep } from '../common/helpers';
 
 export default defineComponent({
   data() {
@@ -48,24 +51,37 @@ export default defineComponent({
     };
   },
   methods: {
-    loadJobs() {
+    async loadJobs() {
+      const fetchTime = timer();
+      fetchTime.start();
       this.isLoading = true;
-      fetch(`${import.meta.env.VITE_FIREBASE_URL}/jobs.json`)
-        .then((response) => {
-          if (!response.ok) throw new Error('Response not OK');
-          return response.json();
-        })
-        .then((data) => {
-          this.isLoading = false;
-          this.jobs = data;
-          console.log('data:', data);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_FIREBASE_URL}/jobs.json`,
+        );
+        const data = await response.json();
+        const stopTime = fetchTime.stop();
+
+        if (!response.ok) throw new Error('Response not OK');
+        if (stopTime < 500) await sleep(500 - stopTime);
+
+        this.isLoading = false;
+        this.jobs = Object.keys(data).map(id => ({ ...data[id], id }));
+      } catch (error) {
+        console.error(error);
+        this.isLoading = false;
+      }
+    },
+    async removeJob(id: string) {
+      const response = await fetch(
+        `${import.meta.env.VITE_FIREBASE_URL}/jobs/${id}.json`,
+        { method: 'DELETE' },
+      );
+      this.jobs = this.jobs.filter((job) => job.id !== id);
     },
   },
-  mounted() {
+  created() {
     this.loadJobs();
   },
 });
